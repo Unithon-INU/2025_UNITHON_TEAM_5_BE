c8d0732bc934b9de4c0d184849e8b847db25d866
jongmine <jongmine.ch@gmail.com>
2025-05-09 00:33:21 +0900

    chore : 초기 설정

---
+++ src/main/java/com/curelingo/curelingo/CurelingoApplication.java
@@ -0,0 +1,13 @@
+package com.curelingo.curelingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurelingoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurelingoApplication.class, args);
+	}
+
+}
