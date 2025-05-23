46d71e077a9e09481e73172f2b37707202a9620e
yuripbong <kyri200350@gmail.com>
2025-05-24 01:23:38 +0900

    feat: 응급의료기관 기본정보조회 연결

--- src/main/java/com/curelingo/curelingo/egen/beds/AvailableBedsController.java
+++ src/main/java/com/curelingo/curelingo/egen/beds/AvailableBedsController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/egen")
+@RequestMapping("/api/egen/beds")
 @RestController
 public class AvailableBedsController {
 
