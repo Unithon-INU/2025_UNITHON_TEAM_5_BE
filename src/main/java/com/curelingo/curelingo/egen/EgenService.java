68edcc78998c898325f26037f4e2073a34c09c2c
jongmine <jongmine.ch@gmail.com>
2025-06-05 01:21:07 +0900

    refactor: baseurl 변경

--- src/main/java/com/curelingo/curelingo/egen/EgenService.java
+++ src/main/java/com/curelingo/curelingo/egen/EgenService.java
@@ -26,7 +26,7 @@ public class EgenService {
     private final RestTemplate restTemplate;
 
     public EgenResponse<NearbyHospitalItem> getNearbyHospitals(double lat, double lon) {
-        String url = String.format("%s/getEgytLcinfoInqire?serviceKey=%s&WGS84_LAT=%s&WGS84_LON=%s&pageNo=1&numOfRows=10&_type=json",
+        String url = String.format("%s/ErmctInfoInqireService/getEgytLcinfoInqire?serviceKey=%s&WGS84_LAT=%s&WGS84_LON=%s&pageNo=1&numOfRows=10&_type=json",
                 baseUrl, apiKey,
                 URLEncoder.encode(String.valueOf(lat), StandardCharsets.UTF_8),
                 URLEncoder.encode(String.valueOf(lon), StandardCharsets.UTF_8)
@@ -35,7 +35,7 @@ public class EgenService {
     }
 
     public EgenResponse<HospitalInfoItem> getHospitalInfo(String hpid) {
-        String url = String.format("%s/getEgytBassInfoInqire?serviceKey=%s&HPID=%s&pageNo=1&numOfRows=10&_type=json",
+        String url = String.format("%s/ErmctInfoInqireService/getEgytBassInfoInqire?serviceKey=%s&HPID=%s&pageNo=1&numOfRows=10&_type=json",
                 baseUrl, apiKey,
                 URLEncoder.encode(hpid, StandardCharsets.UTF_8)
         );
@@ -43,7 +43,7 @@ public class EgenService {
     }
 
     public EgenResponse<AvailableBedsItem> getAvailableBeds(String stage1, String stage2) {
-        String url = String.format("%s/getEmrrmRltmUsefulSckbdInfoInqire?serviceKey=%s&STAGE1=%s&STAGE2=%s&pageNo=1&numOfRows=10&_type=json",
+        String url = String.format("%s/ErmctInfoInqireService/getEmrrmRltmUsefulSckbdInfoInqire?serviceKey=%s&STAGE1=%s&STAGE2=%s&pageNo=1&numOfRows=10&_type=json",
                 baseUrl, apiKey,
                 URLEncoder.encode(stage1, StandardCharsets.UTF_8),
                 URLEncoder.encode(stage2, StandardCharsets.UTF_8)
